f6db77c7f17a3d5ae68b89d5dd9096021c6837a0
Stefan <stlm@posteo.de>
2021-11-05 20:32:41 +0100

    server and capture on demand

--- raspberrypi/serve.py
+++ raspberrypi/serve.py
@@ -1,4 +1,12 @@
-from flask import Flask, url_for, render_template
+from io import BytesIO
+from time import sleep
+from flask import Flask, make_response, url_for, render_template
+import picamera
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.rotation=180
+camera.start_preview()
 
 
 app = Flask(__name__)
@@ -7,6 +15,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/image")
+def serve_image():
+    with BytesIO() as stream:
+        stream = BytesIO()
+        camera.capture(stream, "jpeg")
+        response = make_response(stream.getvalue())
+
+    response.headers.set("Content-type", "image/jpeg")
+    return response
 
 
 app.run("0.0.0.0", 80)
