75757186cddc19fb16c0c4762a6c4d756bcb6ba9
Stefan Lehmann <stlm@posteo.de>
2021-11-12 21:53:01 +0100

    Add image-label association

--- dashapp/db.py
+++ dashapp/db.py
@@ -1,7 +1,7 @@
 import os
 
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
@@ -10,8 +10,24 @@ Session = sessionmaker(engine)
 Base = declarative_base()
 
 
+image_label_table = Table(
+    "image_labels",
+    Base.metadata,
+    Column("image_id", ForeignKey("images.id")),
+    Column("label_id", ForeignKey("labels.id")),
+)
+
+
 class Label(Base):
     __tablename__ = "labels"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False, unique=True)
+
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(254), unique=True, nullable=False)
+    labels = relationship("Label", secondary=image_label_table)
